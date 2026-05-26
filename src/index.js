90d401b512671087eaf111402d1f322e487c05e4
VulaniMap <vulanimaps@gmail.com>
2026-05-26 17:14:00 +0200

    install axios

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function showPoem(response) {
+  console.log("poem generator");
   new Typewriter("#poem", {
-    strings: "The poem goes here",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let topic = document.querySelector("#topic").value;
+  let apiKey = "aofcd5541add57c0396398488b47at43";
+  let context =
+    "You are a poet who writes playful and humorous poems for all ages.";
+  let prompt = `Generate a 5-6 lines of a poem about ${topic}.`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("generating poem");
+
+  axios.get(apiUrl).then(showPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator");

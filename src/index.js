5163e80b81e968c9daaffa6827b23c23776138fe
VulaniMap <vulanimaps@gmail.com>
2026-05-26 16:11:40 +0200

    Add basic Javascript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "The poem goes here",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator");
+poemFormElement.addEventListener("submit", generatePoem);
